4fb9d633f61b641504ae89a25e0099c2c99f4dae
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-24 21:41:52 -0500

    Create minimax_no_pruning.cs

---
+++ minimax_no_pruning.cs
@@ -0,0 +1,92 @@
+using ChessChallenge.API;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MyBot : IChessBot
+{
+    int[] pieceValues = {0, 1, 3, 3, 5, 9, 127};
+    Random rand = new Random();
+
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] legalMoves = board.GetLegalMoves();
+        List<double> values = new List<double>();
+
+        foreach (Move move in legalMoves)
+        {
+            board.MakeMove(move);
+            values.Add(Minimax(board, 2));
+            board.UndoMove(move);
+        }
+
+        if (board.IsWhiteToMove)
+        {
+            return legalMoves[values.IndexOf(values.Max())];
+        }
+
+        return legalMoves[values.IndexOf(values.Min())];
+    }
+
+    double Minimax(Board board, int depth)
+    {
+        if (board.IsInCheckmate())
+        {
+            return (Convert.ToInt32(board.IsWhiteToMove) - 0.5) * -255;
+        }
+
+        if (board.IsDraw())
+        {
+            return 0;
+        }
+
+        if (depth == 0)
+        {
+            return EvaluatePosition(board);
+        }
+
+        Move[] legalMoves = board.GetLegalMoves();
+        List<double> values = new List<double>();
+
+        foreach (Move move in legalMoves)
+        {
+            board.MakeMove(move);
+            values.Add(Minimax(board, depth - 1));
+            board.UndoMove(move);
+        }
+
+        if (board.IsWhiteToMove)
+        {
+            return values[values.IndexOf(values.Max())];
+        }
+
+        return values[values.IndexOf(values.Min())];
+    }
+
+    double EvaluatePosition(Board board)
+    {
+        PieceList[] piecesOnBoard = board.GetAllPieceLists();
+        int noise = rand.Next(0, 99);
+        double eval = (double)noise / 100 - 0.5;
+
+        if (board.IsInCheck())
+        {
+            eval -= (Convert.ToInt32(board.IsWhiteToMove) - 0.5) * 31;
+        }
+
+        foreach (PieceList pieceList in piecesOnBoard)
+        {
+            if (pieceList.IsWhitePieceList)
+            {
+                eval += pieceValues[(int)pieceList[0].PieceType] * pieceList.Count;
+            }
+            else
+            {
+                eval -= pieceValues[(int)pieceList[0].PieceType] * pieceList.Count;   
+            }
+        }
+
+        return Math.Round(eval, 2);
+    }
+}
