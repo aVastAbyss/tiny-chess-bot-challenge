067958bd2876ea3ec3fd060bcbf9451d705c1fb2
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-27 17:09:18 -0500

    Create minimax-pruning-ordering.cs

---
+++ tree-search-functions/minimax-pruning-ordering.cs
@@ -0,0 +1,95 @@
+double Minimax(Board board, double alpha, double beta, int depth)
+{
+    numOfNodesVisited++;
+
+    if (board.IsInCheckmate())
+    {
+        return (Convert.ToInt32(board.IsWhiteToMove) - 0.5) * -64 * (depth + 1);
+    }
+
+    if (board.IsDraw())
+    {
+        return 0;
+    }
+
+    if (depth == 0)
+    {
+        return EvaluateBoard(board);
+    }
+
+    Move[] orderedMoves = GetOrderedMoves(board);
+    double eval;
+
+    if (board.IsWhiteToMove)
+    {
+        double maxEval = -128;
+        foreach (Move move in orderedMoves)
+        {
+            board.MakeMove(move);
+
+            eval = Minimax(board, alpha, beta, depth - 1);
+            maxEval = Math.Max(eval, maxEval);
+            alpha = Math.Max(alpha, eval);
+
+            board.UndoMove(move);
+
+            if (beta <= alpha)
+            {
+                break;
+            }
+        }
+
+        return maxEval;
+    }
+    else
+    {
+        double minEval = 128;
+        foreach (Move move in orderedMoves)
+        {
+            board.MakeMove(move);
+
+            eval = Minimax(board, alpha, beta, depth - 1);
+            minEval = Math.Min(eval, minEval);
+            beta = Math.Min(beta, eval);
+
+            board.UndoMove(move);
+
+            if (beta <= alpha)
+            {
+                break;
+            }
+        }
+
+        return minEval;
+    }
+}
+
+Move[] GetOrderedMoves(Board board)
+{
+    Move[] legalMoves = board.GetLegalMoves();
+
+    int[] moveValues = new int[legalMoves.Length];
+    Move[] movesToSort = new Move[legalMoves.Length];
+
+    for (int i = 0; i < legalMoves.Length; i++)
+    {
+        Move move = legalMoves[i];
+
+        moveValues[i] = 0;
+        movesToSort[i] = move;
+
+        Piece attackingPiece = board.GetPiece(move.StartSquare);
+        Piece capturedPiece = board.GetPiece(move.TargetSquare);
+
+        moveValues[i] += pieceValues[(int)capturedPiece.PieceType];
+
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+        {
+            moveValues[i] -= pieceValues[(int)attackingPiece.PieceType];
+        }
+    }
+
+    Array.Sort(moveValues, movesToSort);
+    Array.Reverse(movesToSort);
+    return movesToSort;
+}
