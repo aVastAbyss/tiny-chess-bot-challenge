7af7dd5d0d6d73d85eb22b5dfbc2a0f463e6acf6
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-28 17:00:19 -0500

    Create negamax-pruning-ordering.cs

---
+++ tree-search-functions/minimax/negamax-pruning-ordering.cs
@@ -0,0 +1,77 @@
+// numOfNodesVisited is a globally defined integer that stores the number of nodes visited
+// alpha and beta are initialized to -128 and 128 respectively
+// pieceValues is a globally defined array that contains the values of each type of piece
+
+// this uses a variation of minimax called negamax for token optimization
+double Negamax(Board board, double alpha, double beta, int depth)
+{
+    numOfNodesVisited++;
+    int colorValue = (int)((Convert.ToInt32(board.IsWhiteToMove) - 0.5) * 2);
+
+    if (board.IsInCheckmate())
+    {
+        return -colorValue * 32 * (depth + 1);
+    }
+
+    if (board.IsDraw())
+    {
+        return 0;
+    }
+
+    if (depth == 0)
+    {
+        return EvaluateBoard(board);
+    }
+
+    Move[] orderedMoves = GetOrderedMoves(board);
+    double eval;
+    double maxEval = -128;
+
+    foreach (Move move in orderedMoves)
+    {
+        board.MakeMove(move);
+
+        eval = colorValue * Negamax(board, -beta, -alpha, depth - 1);
+        maxEval = Math.Max(eval, maxEval);
+        alpha = Math.Max(alpha, eval);
+
+        board.UndoMove(move);
+
+        if (beta <= alpha)
+        {
+            break;
+        }
+    }
+
+    return colorValue * maxEval;
+}
+
+Move[] GetOrderedMoves(Board board)
+{
+    Move[] legalMoves = board.GetLegalMoves();
+
+    int[] moveValues = new int[legalMoves.Length];
+    Move[] movesArray = new Move[legalMoves.Length];
+
+    for (int i = 0; i < legalMoves.Length; i++)
+    {
+        Move move = legalMoves[i];
+
+        moveValues[i] = 0;
+        movesArray[i] = move;
+
+        Piece attackingPiece = board.GetPiece(move.StartSquare);
+        Piece capturedPiece = board.GetPiece(move.TargetSquare);
+
+        moveValues[i] += pieceValues[(int)capturedPiece.PieceType];
+
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+        {
+            moveValues[i] -= pieceValues[(int)attackingPiece.PieceType];
+        }
+    }
+
+    Array.Sort(moveValues, movesArray);
+    Array.Reverse(movesArray);
+    return movesArray;
+}
